36852582522221ea8ce2000ac85d80aba232fa3d
IbrahimTahaSanli <ibrahimtahasanli@gmail.com>
2021-12-05 20:21:30 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include <Windows.h>
+
+typedef const char* getStr(int);
+
+int _stdcall WinMain() {
+	HMODULE module = LoadLibrary(L"opengl32.dll");
+	getStr* glGetString = (getStr*)GetProcAddress(module, "glGetString");
+	//getStr* glGetString = (getStr*)wglGetProcAddress("glGetString"); <= For OpenGL
+
+	return 0;
+
+}
